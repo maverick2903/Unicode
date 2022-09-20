76af5ae08a2ed0d6121fdafd18e0d6320b1b6afc
Varun Viswanath <varunvis2903@gmail.com>
2022-09-20 22:21:30 +0530

    Portfolio - Added finishing touches and improved responsivity of page  Contact - Added time delay in verification  CovidApi - Added Sticky heading

--- Contact.js
+++ Contact.js
@@ -5,37 +5,43 @@ let submitCheck_n = 1;
 let submitCheck_a = 1;
 
 function fnameFunc(nameCheck) {
-  let fName = document.getElementById(nameCheck).value;
-  let extra_ctr = nameCheckFunc(fName);
-  if (fName.length < 2 || extra_ctr != 0) {
-    document.getElementById("fname-error").innerHTML = "Invalid name input";
-    document.getElementById("fname-error").style.cssText =
-      "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
-    document.getElementById("fname-border").style.cssText =
-      "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
-    submitCheck_fn = 1;
-  } else {
-    document.getElementById("fname-error").innerHTML = "";
-    document.getElementById("fname-border").style.cssText = "border:none;";
-    submitCheck_fn = 0;
-  }
+  setTimeout(function () {
+    let fName = document.getElementById(nameCheck).value;
+    let extra_ctr = nameCheckFunc(fName);
+    let fnameError = document.getElementById("fname-error");
+    if (fName.length < 2 || extra_ctr != 0) {
+      fnameError.innerHTML = "Invalid name input";
+      fnameError.style.cssText =
+        "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
+      document.getElementById("fname-border").style.cssText =
+        "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
+      submitCheck_fn = 1;
+    } else {
+      fnameError.innerHTML = "";
+      document.getElementById("fname-border").style.cssText = "border:none;";
+      submitCheck_fn = 0;
+    }
+  }, 500);
 }
 
 function lnameFunc(nameCheck) {
-  let lName = document.getElementById(nameCheck).value;
-  let extra_ctr = nameCheckFunc(lName);
-  if (lName.length < 2 || extra_ctr != 0) {
-    document.getElementById("lname-error").innerHTML = "Invalid name input";
-    document.getElementById("lname-error").style.cssText =
-      "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
-    document.getElementById("lname-border").style.cssText =
-      "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
-    submitCheck_ln = 1;
-  } else {
-    document.getElementById("lname-error").innerHTML = "";
-    document.getElementById("lname-border").style.cssText = "border:none;";
-    submitCheck_ln = 0;
-  }
+  setTimeout(function () {
+    let lName = document.getElementById(nameCheck).value;
+    let extra_ctr = nameCheckFunc(lName);
+    let lnameError = document.getElementById("lname-error");
+    if (lName.length < 2 || extra_ctr != 0) {
+      lnameError.innerHTML = "Invalid name input";
+      lnameError.style.cssText =
+        "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
+      document.getElementById("lname-border").style.cssText =
+        "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
+      submitCheck_ln = 1;
+    } else {
+      lnameError.innerHTML = "";
+      document.getElementById("lname-border").style.cssText = "border:none;";
+      submitCheck_ln = 0;
+    }
+  }, 500);
 }
 
 function nameCheckFunc(nameBoth) {
@@ -50,69 +56,78 @@ function nameCheckFunc(nameBoth) {
 }
 
 function emailFunc() {
-  let email = document.getElementById("email-check").value;
-  if (
-    email.indexOf("@") > 1 &&
-    email.indexOf(".") > 0 &&
-    email.indexOf(".") < email.length - 1 &&
-    email.indexOf(".") >= email.indexOf("@") + 1
-  ) {
-    document.getElementById("email-error").innerHTML = "Valid email input";
-    document.getElementById("email-error").style.cssText =
-      "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
-    document.getElementById("email-border").style.cssText =
-      "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
-    submitCheck_e = 0;
-  } else {
-    document.getElementById("email-error").innerHTML = "Invalid email input";
-    document.getElementById("email-error").style.cssText =
-      "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
-    document.getElementById("email-border").style.cssText =
-      "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
-    submitCheck_e = 1;
-  }
+  setTimeout(function () {
+    let email = document.getElementById("email-check").value;
+    let emailError = document.getElementById("email-error");
+    if (
+      email.indexOf("@") > 1 &&
+      email.indexOf(".") > 0 &&
+      email.indexOf(".") < email.length - 1 &&
+      email.indexOf(".") >= email.indexOf("@") + 1
+    ) {
+      emailError.innerHTML = "Valid email input";
+      emailError.style.cssText =
+        "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
+      document.getElementById("email-border").style.cssText =
+        "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
+      submitCheck_e = 0;
+    } else {
+      emailError.innerHTML = "Invalid email input";
+      emailError.style.cssText =
+        "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
+      document.getElementById("email-border").style.cssText =
+        "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
+      submitCheck_e = 1;
+    }
+  }, 500);
 }
 
 function telephoneFunc() {
-  let tele = parseInt(document.getElementById("tel-check").value);
-  if (
-    (tele > 1000000000 && tele < 9999999999) ||
-    (tele > 10000000 && tele < 99999999)
-  ) {
-    console.log(tele);
-    document.getElementById("tel-error").innerHTML = "Valid telephone number";
-    document.getElementById("tel-error").style.cssText =
-      "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
-    document.getElementById("tel-border").style.cssText =
-      "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
-    submitCheck_n = 0;
-  } else {
-    document.getElementById("tel-error").innerHTML = "Invalid telephone number";
-    document.getElementById("tel-error").style.cssText =
-      "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
-    document.getElementById("tel-border").style.cssText =
-      "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
-    submitCheck_n = 1;
-  }
+  setTimeout(function () {
+    let tele = parseInt(document.getElementById("tel-check").value);
+    let telError = document.getElementById("tel-error");
+    if (
+      (tele > 1000000000 && tele < 9999999999) ||
+      (tele > 10000000 && tele < 99999999)
+    ) {
+      console.log(tele);
+      telError.innerHTML = "Valid telephone number";
+      telError.style.cssText =
+        "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
+      document.getElementById("tel-border").style.cssText =
+        "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
+      submitCheck_n = 0;
+    } else {
+      telError.innerHTML = "Invalid telephone number";
+      telError.style.cssText =
+        "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
+      document.getElementById("tel-border").style.cssText =
+        "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
+      submitCheck_n = 1;
+    }
+  }, 500);
 }
 
 function ageFunc() {
-  let age = parseInt(document.getElementById("age-check").value);
-  if (age > 10 && age < 120) {
-    document.getElementById("age-error").innerHTML = "Valid age";
-    document.getElementById("age-error").style.cssText =
-      "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
-    document.getElementById("age-border").style.cssText =
-      "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
-    submitCheck_a = 0;
-  } else {
-    document.getElementById("age-error").innerHTML = "Invalid age";
-    document.getElementById("age-error").style.cssText =
-      "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
-    document.getElementById("age-border").style.cssText =
-      "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
-    submitCheck_a = 1;
-  }
+  setTimeout(function () {
+    let age = parseInt(document.getElementById("age-check").value);
+    let ageError = document.getElementById("age-error");
+    if (age > 10 && age < 120) {
+      ageError.innerHTML = "Valid age";
+      ageError.style.cssText =
+        "color:rgb(15, 97, 14);font-weight:700;font-size:17px";
+      document.getElementById("age-border").style.cssText =
+        "border:2px solid rgb(15, 97, 14);display:inline-block;border-radius:12px;";
+      submitCheck_a = 0;
+    } else {
+      ageError.innerHTML = "Invalid age";
+      ageError.style.cssText =
+        "color:rgb(176, 11, 11);font-weight:700;font-size:17px";
+      document.getElementById("age-border").style.cssText =
+        "border:2px solid rgb(176, 11, 11);display:inline-block;border-radius:12px;";
+      submitCheck_a = 1;
+    }
+  }, 500);
 }
 
 function submitFunc() {
